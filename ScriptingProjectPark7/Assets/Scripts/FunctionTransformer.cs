ad40348c9532cb009112ad5f2f5dc651b0b75224
Hyunmin Park <753429@student.dusd.net>
2024-01-31 14:38:28 -0800

    translate and rotate
    made a cube and coded it to translate (move around) and rotate on a plane. made the cube die

---
+++ ScriptingProjectPark7/Assets/Scripts/FunctionTransformer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionTransformer : MonoBehaviour
+{
+    //Variables:
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        }
+    }
+}

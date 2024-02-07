dccc84c226899b8eb6ee769f5d81d72ccd83c02d
Hyunmin Park <753429@student.dusd.net>
2024-02-07 15:11:03 -0800

    added linear interpolation
    linear interpolation as a comment

---
+++ ScriptingProjectPark7/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this code increases the light intensity every frame update.
+        goes from 0, then 4, and onwards.
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+        
+        //This code does the same thing but every second.
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    }
+    //The result would be 25. Hopefully. 
+    25 is 50% between 0 and 50. 0.5f is equal to 50%
+    float result = Mathf.Lerp (0f, 50f, 0.5f);
+
+    Vector3 from = new Vector3 (1f, 2f, 3f); 
+    Vector3 to = new Vector3 (5f, 6f, 7f);
+
+    //Result = (4, 5, 6). 
+    This is because each number is 75% of the way between the numbers. 
+    Vector3 result = Vector3.Lerp (from, to, 0.75f);
+
+
+    */
+
+
+}

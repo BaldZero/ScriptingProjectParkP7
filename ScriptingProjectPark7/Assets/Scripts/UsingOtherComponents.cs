ff6c6eae98736d3ab0d40974d26f4e8fb48630d2
Hyunmin Park <753429@student.dusd.net>
2024-03-14 14:30:20 -0700

    GetComponent
    Made UsingOtherScript, AnotherScript, and AnotherScriptSquared. Learned how to use other components/scripts

---
+++ ScriptingProjectPark7/Assets/Scripts/UsingOtherComponents.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingOtherComponents : MonoBehaviour
+{
+
+    public GameObject otherBoi;
+
+        private AnotherScript anotherScript;
+        private AnotherScriptSquared anotherScriptSquared;
+        private BoxCollider boxCol;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anotherScript = GetComponent<AnotherScript>();
+        anotherScriptSquared = GetComponent<AnotherScriptSquared>();
+        boxCol = GetComponent<BoxCollider>();
+    }
+
+    void Start()
+    {
+        boxCol.size = new Vector3(3, 3, 3);
+        Debug.Log("The player's score is " + anotherScript.playerScore);
+        Debug.Log("The player has died " + anotherScriptSquared.numberOfPlayerDeaths + " times");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

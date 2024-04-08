e7c03be6256b42249b1ca03b0390937a83466038
Hyunmin Park <753429@student.dusd.net>
2024-04-08 14:50:22 -0700

    Classes
    cube go shooting capsule and i leearned aabout classes

---
+++ ScriptingProjectPark7/Assets/Scripts/MovementControls.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControls : MonoBehaviour
+{
+
+    public float speed;
+    public float turnSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float turnMovement = Input.GetAxis("Horizontaal") * turnSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * forwardMovement);
+        transform.Rotate(Vector3.forward * turnMovement);
+    }
+}

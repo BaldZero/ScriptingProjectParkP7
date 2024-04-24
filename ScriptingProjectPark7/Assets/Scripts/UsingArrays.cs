3b55af4aabee9691a2e3596d32f0a33009b0ff92
Hyunmin Park <753429@student.dusd.net>
2024-04-24 14:20:54 -0700

    Arrays
    used arrays

---
+++ ScriptingProjectPark7/Assets/Scripts/UsingArrays.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class UsingArrays : MonoBehaviour
+{
+    public GameObject[] player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player");
+
+        for (int i = 0; i < player.Length; i++)
+        {
+            Debug.Log("Player Number " + i + " is named " + player[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
